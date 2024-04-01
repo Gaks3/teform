a1556bff43f9f820058b8f53717d96944341ebf4
Gaks3 <dwi66116@gmail.com>
2024-04-01 11:01:09 +0700

    feat: authentication with lucia

---
+++ src/lib/auth.ts
@@ -0,0 +1,68 @@
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma'
+import { Lucia } from 'lucia'
+import prisma from './db'
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user)
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      username: attributes.username,
+      email: attributes.email,
+      isAdmin: attributes.isAdmin,
+    }
+  },
+})
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia
+    DatabaseUserAttributes: DatabaseUserAttributes
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string
+  username: string
+  email: string
+  isAdmin: boolean
+}
+
+import { cookies } from 'next/headers'
+import { cache } from 'react'
+import { redirect } from 'next/navigation'
+
+export const getUser = cache(async () => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
+  if (!sessionId) return null
+
+  const { user, session } = await lucia.validateSession(sessionId)
+
+  try {
+    if (session && session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id)
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      )
+    }
+
+    if (!session) {
+      const sessionCookie = lucia.createBlankSessionCookie()
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      )
+    }
+  } catch {
+    redirect('/sign-up')
+  }
+
+  return { user, session }
+})
