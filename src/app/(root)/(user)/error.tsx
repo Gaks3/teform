8524fddeac0cbbb25a504c534aec6e3d58fb79ff
Gaks3 <dwi66116@gmail.com>
2024-04-02 09:08:02 +0700

    feat: error page

---
+++ src/app/(root)/(user)/error.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function ErrorPage() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <h1>Something Went Wrong</h1>
+      <p>Please try again later</p>
+    </div>
+  )
+}
