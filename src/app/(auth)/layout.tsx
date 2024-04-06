92bff3f4d580d416ea39756e3ac0b5bb23c90d77
Gaks3 <dwi66116@gmail.com>
2024-04-06 15:07:14 +0700

    feat: get user in auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,14 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { getUser } from '@/lib/auth'
+import { redirect } from 'next/navigation'
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const user = await getUser()
+  if (user?.user) return redirect('/')
+
   return (
     <div className='flex flex-col items-center justify-center w-full min-h-screen gap-5'>
       {children}
