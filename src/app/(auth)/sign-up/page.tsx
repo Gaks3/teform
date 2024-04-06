515305024e4bcccd407b2c9d3df39bc9c4a4b7e4
Gaks3 <dwi66116@gmail.com>
2024-04-06 15:08:31 +0700

    feat: link to sign in page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,18 +3,15 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { getUser } from '@/lib/auth'
-import { redirect } from 'next/navigation'
+import Link from 'next/link'
 
 export default async function Page() {
-  const user = await getUser()
-  if (user) return redirect('/')
-
   return (
-    <Card className='min-w-96'>
+    <Card className='min-w-80 md:min-w-96'>
       <CardHeader>
         <CardTitle>Sign Up</CardTitle>
         <CardDescription>Create your account</CardDescription>
@@ -22,6 +19,14 @@ export default async function Page() {
       <CardContent>
         <SignUp />
       </CardContent>
+      <CardFooter>
+        <span className='text-sm'>
+          Already have account?{' '}
+          <Link href={'/sign-in'} className='underline'>
+            Sign In here
+          </Link>
+        </span>
+      </CardFooter>
     </Card>
   )
 }
