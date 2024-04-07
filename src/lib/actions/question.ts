c9a99ae8205522a638106908f4bd786c3a9bf7a4
Gaks3 <dwi66116@gmail.com>
2024-04-07 18:43:20 +0700

    feat: crud question

--- src/lib/actions/question.ts
+++ src/lib/actions/question.ts
@@ -24,18 +24,57 @@ export async function UpdateContent(
   )
   const ids = updateElement.map(({ idElement }) => idElement)
 
-  await prisma.question.deleteMany({
+  const questionInDatabase = await prisma.question.findMany({
     where: {
+      formId: id,
       idElement: {
         in: ids,
       },
-      formId: id,
     },
   })
 
-  await prisma.question.createMany({
-    data: updateElement,
+  const existUpdate: { data: Prisma.QuestionUpdateInput; id: number }[] =
+    updateElement
+      .filter(({ idElement }) => {
+        const data = questionInDatabase.find(
+          ({ idElement: id }) => idElement === id
+        )
+
+        return idElement === data?.idElement
+      })
+      .map((data) => {
+        const question = questionInDatabase.find(
+          ({ idElement }) => idElement === data.idElement
+        )
+
+        return { data: { ...data }, id: question!.id }
+      })
+
+  const notExitstUpdate = updateElement.filter(({ idElement }) => {
+    const data = questionInDatabase.find(
+      ({ idElement: id }) => idElement === id
+    )
+
+    return idElement !== data?.idElement
   })
+
+  if (existUpdate.length > 0)
+    await Promise.all(
+      existUpdate.map(async (data) => {
+        await prisma.question.update({
+          data: data.data,
+          where: {
+            id: data.id,
+          },
+        })
+      })
+    )
+
+  if (notExitstUpdate.length > 0)
+    await prisma.question.createMany({
+      data: notExitstUpdate,
+      skipDuplicates: true,
+    })
 }
 
 export async function GetQuestionsByFormId(id: string) {
@@ -58,5 +97,87 @@ export async function GetQuestionsByFormId(id: string) {
       },
       questionId: id,
     } as FormElementInstance & { questionId: number }
+  })
+}
+
+export async function UpdateQuestionById(
+  formId: string,
+  data: FormElementInstance
+) {
+  const formatData: Prisma.QuestionUpdateInput = {
+    label: data.extraAttributes!.label,
+    required: data.extraAttributes!.required,
+    type: data.type,
+    idElement: data.id,
+    ...(data.extraAttributes?.option && {
+      option: data.extraAttributes.option,
+    }),
+  }
+
+  await prisma.question.update({
+    where: {
+      formId_idElement: {
+        formId,
+        idElement: data.id,
+      },
+    },
+    data: formatData,
+  })
+}
+
+export async function RemoveQuestion(formId: string, idElement: string) {
+  await prisma.question.delete({
+    where: {
+      formId_idElement: {
+        formId,
+        idElement,
+      },
+    },
+  })
+}
+
+export async function CreateQuestion(
+  formId: string,
+  data: FormElementInstance
+) {
+  const formatData: Prisma.QuestionCreateInput = {
+    label: data.extraAttributes!.label,
+    required: data.extraAttributes!.required,
+    type: data.type,
+    idElement: data.id,
+    formId,
+    ...(data.extraAttributes?.option && {
+      option: data.extraAttributes.option,
+    }),
+  }
+
+  await prisma.question.create({
+    data: formatData,
+  })
+}
+
+export async function UpdateTypeQuestion(
+  formId: string,
+  prevId: string,
+  data: FormElementInstance
+) {
+  const formatData: Prisma.QuestionCreateInput = {
+    label: data.extraAttributes!.label,
+    required: data.extraAttributes!.required,
+    type: data.type,
+    idElement: data.id,
+    ...(data.extraAttributes?.option && {
+      option: data.extraAttributes.option,
+    }),
+  }
+
+  await prisma.question.update({
+    data: formatData,
+    where: {
+      formId_idElement: {
+        formId,
+        idElement: prevId,
+      },
+    },
   })
 }
