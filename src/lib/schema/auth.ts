d544fe0dde236f350326b65ff23ed79f52a970fa
Gaks3 <dwi66116@gmail.com>
2024-04-07 18:43:59 +0700

    feat: validation email if exist

--- src/lib/schema/auth.ts
+++ src/lib/schema/auth.ts
@@ -4,54 +4,70 @@ import {
   containsSpecialChars,
   containsUppercase,
 } from '@/lib/utils'
+import { GetUserByEmail } from '../actions/user'
 
-export const signUpSchema = z.object({
-  email: z.string().email('Not a valid email'),
-  username: z
-    .string()
-    .min(2, { message: 'Username must be at least 2 characters' }),
-  password: z.string().superRefine((value, ctx) => {
-    if (value.length < 8) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Must be 8 or more characters long',
-        fatal: true,
-      })
+export const signUpSchema = z
+  .object({
+    email: z
+      .string()
+      .email('Not a valid email')
+      .refine(async (value) => {
+        const exist = await GetUserByEmail(value)
 
-      return z.NEVER
-    }
+        if (exist) return false
 
-    if (!containsUppercase(value)) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'At least contains one uppercase letter',
-        fatal: true,
-      })
+        return true
+      }),
+    username: z
+      .string()
+      .min(2, { message: 'Username must be at least 2 characters' }),
+    password: z.string().superRefine((value, ctx) => {
+      if (value.length < 8) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'Must be 8 or more characters long',
+          fatal: true,
+        })
 
-      return z.NEVER
-    }
+        return z.NEVER
+      }
 
-    if (!containsNumber(value)) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'At least contains one number',
-        fatal: true,
-      })
+      if (!containsUppercase(value)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least contains one uppercase letter',
+          fatal: true,
+        })
 
-      return z.NEVER
-    }
+        return z.NEVER
+      }
 
-    if (!containsSpecialChars(value)) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'At least contains one special characters (@, #, $, etc.)',
-        fatal: true,
-      })
+      if (!containsNumber(value)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least contains one number',
+          fatal: true,
+        })
 
-      return z.NEVER
-    }
-  }),
-})
+        return z.NEVER
+      }
+
+      if (!containsSpecialChars(value)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least contains one special characters (@, #, $, etc.)',
+          fatal: true,
+        })
+
+        return z.NEVER
+      }
+    }),
+    confirm: z.string(),
+  })
+  .refine((values) => values.password === values.confirm, {
+    message: "Password don't match",
+    path: ['confirm'],
+  })
 
 export const signInSchema = z.object({
   email: z.string().email('Email is not valid'),
